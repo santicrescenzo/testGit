19550bfa98eb9c1dbffe1c38f079c487822ce487
santicrescenzo <santicrescenzo7@gmail.com>
2024-08-27 21:59:14 -0300

    Test con metodo SUMA

--- Test.java
+++ Test.java
@@ -4,10 +4,13 @@ public class Test {
 	public static void main(String[] args) {
 		int a = 2;
 		int b = 3;
-		
-		int resultado = a + b;
-		
+
+		int resultado = suma(a, b);
+
 		System.out.println(resultado);
 	}
 
+	private static int suma(int a, int b) {
+		return a + b;
+	}
 }
